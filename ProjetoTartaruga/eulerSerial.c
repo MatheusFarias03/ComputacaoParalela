36f523d5225c0f7489fac45eeaf579c1e7992011
Leonardo <leonardo01ps@gmail.com>
2023-04-26 11:49:07 +0000

    Euler Serial (Max 10)

---
+++ ProjetoTartaruga/eulerSerial.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+long double Factorial(int num) {
+  long double value = 1;
+  for (int i = 1; i <= num; i++) {
+    value *= i;
+  }
+  return value;
+}
+
+long double EulerNum(int n) {
+
+  long double euler = 0;
+  long double one = 1;
+  for (int i = 0; i < n; i++) {
+    euler += one/(Factorial(i));
+    // printf("%Lf", euler);
+  }  
+  return euler;
+}
+
+int main(void) {
+
+  long double num;
+  num = EulerNum(10);
+  printf("\nlong double: %Lf \n", num);
+  
+  return 0;
+}
