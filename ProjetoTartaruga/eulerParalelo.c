01afad4dde8120fefe3408913db87fd1261ec942
Ubuntu <matheusfarias519@gmail.com>
2023-05-04 11:46:51 +0000

    Adicionado num processos

--- ProjetoTartaruga/eulerParalelo.c
+++ ProjetoTartaruga/eulerParalelo.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 
 int main(int argc, char* argv[]) {
 
-	printf("Hello from process %d", omp_get_thread_num());
+	int thread_count = strtol(argv[1], NULL, 10);
+	#pragma omp parallel num_threads(thread_count)
+	
+	printf("Hello from process %d\n", omp_get_thread_num());
 
 	return 0;
 }
