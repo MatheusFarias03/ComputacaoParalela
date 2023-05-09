7c83cc52cf915996529f639abe552f97af82b881
Leonardo <leonardo01ps@gmail.com>
2023-05-09 22:01:23 +0000

    Euler Paralelo (Erro 29+)

--- ProjetoTartaruga/eulerParalelo.c
+++ ProjetoTartaruga/eulerParalelo.c
@@ -19,9 +19,11 @@ int main(int argc, char* argv[]){
   int start, end; // Valores para saber de onde cada processo comeca e ate onde vai.
   int fd[2]; // File Descriptors. Inteiros que representam um arquivo aberto.
   
-  mpf_t euler, fat;
-  mpf_init2(euler, 33500U);
-  mpf_set_ui(euler,0);
+  mpf_t euler, eulerChild, fat, totalResult;
+  mpf_init2(euler, 3350000U);
+  mpf_init2(fat, 3350000U);
+  mpf_init2(totalResult, 3350000U);
+  mpf_set_ui(totalResult, 1);
         
 	// Caso o pipe nao possa ser criado.
 	if (pipe(fd) == -1)
@@ -35,22 +37,23 @@ int main(int argc, char* argv[]){
     return 1;
 
 	// Processo filho vai de 1 ate o piso da metade do valor que desejamos obter o fatorial.
-  if (id == 0)
-	{ 
+  if (id == 0) { 
     start = 1;
     end = value / 2;
+    // printf("Id 0 -> Start: %d, End: %d\n\n", start, end);
   } 
 	
 	// Processo pai vai do teto da metado do valor ate o valor em si.
-	else 
-	{ 
+  else { 
     start = value / 2 + 1;
     end = value;
+    // printf("Id X -> Start: %d, End: %d\n\n", start, end);
   }
-
-  mpf_init2(fat, 33500U);
+  
   mpf_set_ui(fat, 1);
   mpf_t div;
+  mpf_init2(div, 1);
+  mpf_set_ui(div, 1);
   int i;
   
   for (i = start; i <= end; i++){
@@ -59,35 +62,32 @@ int main(int argc, char* argv[]){
     mpf_add(euler, euler, div);
   }
 
-  gmp_printf("Calculo dos resultados parciais: %.9999Ff\n", euler);
-  // printf("Calculo dos resultados parciais: %Lf\n", euler); 
+  gmp_printf("Calculo dos resultados parciais: %.100Ff\n", euler);
+  // printf("\nCalculo dos resultados parciais: %Lf\n", euler); 
 
-  if (id == 0)
-	{
+  if (id == 0){
     close(fd[0]);
     write(fd[1], &euler, sizeof(euler)); // grava o que foi obtido no processo pai na variavel result
     close(fd[1]);
   } 
 	
   else {
-    mpf_t eulerChild, totalResult, one;
     close(fd[1]);
-    read(fd[0], &eulerChild, sizeof(eulerChild)); // passa o que foi obtido no processo filho para a variavel resultChild
+    read(fd[0], &eulerChild, sizeof(euler)); // passa o que foi obtido no processo filho para a variavel resultChild
     close(fd[0]);
+    
+    mpf_add(totalResult, totalResult, euler);
+    gmp_printf("Total Result + euler: %.15Ff\n", totalResult);
 
-    mpf_init2(one, 1);
-    mpf_set_ui(one, 1);
-    mpf_add(totalResult, one, euler);
-    mpf_add(totalResult, totalResult, eulerChild);
-    gmp_printf("Resultado Final: %.9999Ff\n", totalResult);
-    mpf_clear(eulerChild);
+    gmp_printf("Euler Child: %.15Ff\n", eulerChild);
+   
+    // gmp_printf("Resultado Final: %.9999Ff\n", totalResult);
+  
     mpf_clear(totalResult);
-    mpf_clear(one);
     wait(NULL);
   }
-
+  mpf_clear(euler);
   mpf_clear(div);
   mpf_clear(fat);
-  mpf_clear(euler);
   return 0;
 }
